e3b1589ed582916853def0f35561c3777e24d77c
Pari-Maurya <parimaurya2006@gmail.com>
2026-06-09 13:04:54 +0530

    backend data fetched

--- notification-app-be/src/index.js
+++ notification-app-be/src/index.js
@@ -1,13 +1,31 @@
-import { fetchNotifications } from "./services/notificationService.js";
+import { fetchNotifications }
+from "./services/notificationService.js";
+
+import { calculateScore }
+from "./utils/priorityUtils.js";
 
 async function main() {
-  const notifications = await fetchNotifications();
 
-  console.log(
-    `Fetched ${notifications.length} notifications`
-  );
+  const notifications =
+    await fetchNotifications();
 
-  console.log(notifications);
+  const sortedNotifications =
+    notifications.sort(
+      (a, b) =>
+        calculateScore(b) -
+        calculateScore(a)
+    );
+
+  const top10 =
+    sortedNotifications.slice(0, 10);
+
+  console.log("\nTOP 10 NOTIFICATIONS\n");
+
+  top10.forEach((n, index) => {
+    console.log(
+      `${index + 1}. ${n.Type} | ${n.Message} | ${n.Timestamp}`
+    );
+  });
 }
 
 main();
